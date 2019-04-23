771f99d52c194bfaebe70629dd5dbd0d1ef83507
Rémi Achard <remi.achard@ymagis.com>
2019-04-23 10:44:00 +0200

    Initial import

---
+++ tutorial/tutorial/snippets/urls.py
@@ -0,0 +1,14 @@
+from snippets import views
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet)
+router.register(r'users', views.UserViewSet)
+
+# The API URLs are now determined automatically by the router
+urlpatterns = [
+    path('', include(router.urls)),
+]
